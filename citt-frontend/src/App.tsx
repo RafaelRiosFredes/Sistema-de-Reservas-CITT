5a17f2a637333faa41467f42b87d7caf2ac04ec1
squezada <savkita.quezada@gmail.com>
2026-06-09 13:02:39 -0400

    Implementar frontend del calendario

--- citt-frontend/src/App.tsx
+++ citt-frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { MainLayout } from "./componentes/AppLayout";
+import { AppLayout } from "./componentes/AppLayout";
 import EspaciosPage from "./pages/EspaciosPage";
 import { ReservarEspacioPage } from "./pages/SolicitarReservaPage";
 import { FormularioReservaEspacioPage } from "./pages/FormularioSolicitudPage";
 import { ReservasPage } from "./pages/ReservasPage";
 import { HistorialEspaciosPage } from "./pages/HistorialPage";
+import { CalendarioPage } from "./pages/CalendarioPage";
 
 function App() {
   return (
@@ -14,12 +15,13 @@ function App() {
         <Route path="/" element={<Navigate to="/espacios" replace />} />
 
         {/* RUTAS PRIVADAS (Con el menú lateral) */}
-        <Route element={<MainLayout />}>
+        <Route element={<AppLayout />}>
           <Route path="/espacios" element={<EspaciosPage />} />
           <Route path="/reservar-espacio" element={<ReservarEspacioPage />} />
           <Route path="/formulario-reserva" element={<FormularioReservaEspacioPage />} />
           <Route path="/reservas" element={<ReservasPage />} />
           <Route path="/historial-espacios" element={<HistorialEspaciosPage />} />
+          <Route path="/calendario" element={<CalendarioPage />} />
         </Route>
 
         {/* COMODÍN */}
